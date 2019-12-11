70ee9f348d94f14708009b740e877c1ef24ad492
o-ngtrieuhau861gmailcom <58280240+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2019-12-11 16:21:11 +0700

    cập nhật login

--- forms/login.v.1.0/login.js
+++ forms/login.v.1.0/login.js
@@ -14,14 +14,39 @@ $(document).ready(function () {
             function (data, status) {
                 if (data && data.StatusCode && data.StatusCode == 'OK') {
                     $("#inputMail").val(JSON.stringify(data.Output, null, '\t'));
+                    $("#lblLogin").val('Xin chào, '+user);
+                    setLogin(true);
                 }
                 else if (data && data.Error && data.Error != '') {
                     alert(data.Error);
+                    setLogin(false);
                 }
                 else {
                     $("#inputMail").val("Data: " + JSON.stringify(data, null, '\t') + "\nStatus: " + new Date() + JSON.stringify(status, null, '\t'));
+                    setLogin(false);
                 }
-
             });
     });
 });
+function setLogin(islogin){
+    if(islogin==true){
+        document.getElementById("lblLogin").style = "display:show";
+        document.getElementById("btnSignOut").style = "display:show";
+        
+        document.getElementById("inputMail").style = "display:none"; 
+        document.getElementById("icon-mail").style = "display:none"; 
+        document.getElementById("icon-pass").style = "display:none"; 
+        document.getElementById("inputPassword").style = "display:none"; 
+        document.getElementById("btnSignIn").style = "display:none";
+    }
+    else{
+        document.getElementById("lblLogin").style = "display:show";
+        document.getElementById("btnSignOut").style = "display:show";
+        
+        document.getElementById("inputMail").style = "display:none"; 
+        document.getElementById("inputPassword").style = "display:none"; 
+        document.getElementById("btnSignIn").style = "display:none";
+        document.getElementById("icon-mail").style = "display:show"; 
+        document.getElementById("icon-pass").style = "display:show";
+    }
+}
