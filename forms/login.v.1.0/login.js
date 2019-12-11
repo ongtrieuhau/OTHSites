d51443d7dd82bd852cea58a5c84fcfb59397179a
o-ngtrieuhau861gmailcom <58280240+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2019-12-11 22:56:49 +0700

    c

--- forms/login.v.1.0/login.js
+++ forms/login.v.1.0/login.js
@@ -1,6 +1,11 @@
 function checkLogin() {
     document.getElementById("lblLogin").style = "display:none";
     document.getElementById("btnSignOut").style = "display:none";
+    
+    var date = new Date();
+    date.setTime(date.getTime()+3600);
+    document.cookie = "cookiesoth" + "=" + "cookiesoth" + "; expires=" + date.toGMTString();
+    console.log(document.cookie);
     //alert("Page is loaded");
     //document.getElementById("btnSignOut").style = "display:show";
 }
@@ -13,7 +18,8 @@ $(document).ready(function () {
             { 'Input': input },
             function (data, status) {
                 if (data && data.StatusCode && data.StatusCode == 'OK') {
-                    $("#inputMail").val(JSON.stringify(data.Output, null, '\t'));
+                    document.cookie = 'tokens=thuhang;SameSite=None';
+                    $("#inputMail").val(document.cookie);
                     $("#lblLogin").text('Xin chào, ' + window.atob(user));
                     setLogin(true);
                 }
@@ -35,22 +41,32 @@ $(document).ready(function () {
     });
 });
 function setLogin(islogin) {
+    
+    var date = new Date();
+    date.setTime(date.getTime()+3600);
+    document.cookie = "cookiesoth" + "=" + "cookiesoth" + "; expires=" + date.toGMTString();
+    console.log(document.cookie);
+    
     var showElement = "display:show";
     var hideElement = "display:none";
-    var inputs = ["inputMail", "inputPassword", "icon-mail", "icon-pass", "btnSignIn"];
+    var inputs = ["inputMail777", "inputPassword", "icon-mail", "icon-pass", "btnSignIn"];
     var outputs = ["lblLogin", "btnSignOut"];
     for (var i = 0, ilen = inputs.length; i < ilen; i++) {
         var el = document.getElementById(inputs[i]);
-        if (islogin)
-            el.style = hideElement;
-        else
-            el.style = showElement;
+        if (el) {
+            if (islogin)
+                el.style = hideElement;
+            else
+                el.style = showElement;
+        }
     }
     for (var i = 0, ilen = outputs.length; i < ilen; i++) {
         var el = document.getElementById(outputs[i]);
-        if (islogin)
-            el.style = showElement;
-        else
-            el.style = hideElement;
+        if (el) {
+            if (islogin)
+                el.style = showElement;
+            else
+                el.style = hideElement;
+        }
     }
 }
