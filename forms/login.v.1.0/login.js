89f86df70b4b8404ed6c69e0d63e74ad040f0ff3
o-ngtrieuhau861gmailcom <58280240+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2019-12-11 14:25:19 +0700

    Chỉnh form login

---
+++ forms/login.v.1.0/login.js
@@ -0,0 +1,6 @@
+function checkLogin() {
+    document.getElementById("lblLogin").style = "display:none";
+    document.getElementById("btnSignOut").style = "display:none";
+    alert("Page is loaded");
+    document.getElementById("btnSignOut").style = "display:show";
+}
