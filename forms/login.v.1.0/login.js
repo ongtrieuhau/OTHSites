c0fd6c208bba6e8505520147a06a26d537603837
o-ngtrieuhau861gmailcom <58280240+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2019-12-11 16:05:41 +0700

    Cập nhật login

--- forms/login.v.1.0/login.js
+++ forms/login.v.1.0/login.js
@@ -1,6 +1,27 @@
 function checkLogin() {
     document.getElementById("lblLogin").style = "display:none";
     document.getElementById("btnSignOut").style = "display:none";
-    alert("Page is loaded");
-    document.getElementById("btnSignOut").style = "display:show";
+    //alert("Page is loaded");
+    //document.getElementById("btnSignOut").style = "display:show";
 }
+$(document).ready(function () {
+    $("#btnSignIn").click(function () {
+        var user = window.btoa(document.getElementById("inputMail").value);
+        var pass = window.btoa(document.getElementById("inputPassword").value);
+        var input = window.btoa(user + ':' + pass);
+        $.get("https://script.google.com/macros/s/AKfycbwEc5wRLVL3nf8FKE46drhQssdMuCPpQlqyerQX/exec",
+            { 'Input': input },
+            function (data, status) {
+                if (data && data.StatusCode && data.StatusCode == 'OK') {
+                    $("#inputMail").val(JSON.stringify(data.Output, null, '\t'));
+                }
+                else if (data && data.Error && data.Error != '') {
+                    alert(data.Error);
+                }
+                else {
+                    $("#inputMail").val("Data: " + JSON.stringify(data, null, '\t') + "\nStatus: " + new Date() + JSON.stringify(status, null, '\t'));
+                }
+
+            });
+    });
+});
